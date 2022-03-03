9b5e4cb0a95cf4d8e414526a04a7aca12bb5c3f2
jlucartc <jlucartc@gmail.com>
2022-03-03 08:26:28 -0300

    Criando views básicas antes de começar os testes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 class ApplicationController < ActionController::Base
 
 	def after_sign_in_path_for(resource)
+		meus_mangas_path
 	end
 
 	def after_sign_out_path_for(resource)
+		new_usuario_session_path
 	end
 
 	def index
-		@posts = Post.all
+		redirect_to new_usuario_session_path
 	end
 
 end
