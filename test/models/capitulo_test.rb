2b6f42f6ab17561481f6dce9abfa491b0c16d789
jlucartc <jlucartc@gmail.com>
2022-03-06 18:30:37 -0300

    Removendo linhas desnecessárias de caso de teste

--- test/models/capitulo_test.rb
+++ test/models/capitulo_test.rb
@@ -2,8 +2,6 @@ require "test_helper"
 
 class CapituloTest < ActiveSupport::TestCase
   test "ao destruir um capítulo, todas as suas imagens devem ser destruídas" do
-    imagens = Imagem.where(capitulo_id: 1)
-    imagens.each{|imagem| imagem.arquivo = pega_conteudo_imagem; imagem.save}
     Capitulo.find(1).destroy
     assert Imagem.where(capitulo_id: 1).empty?
   end
